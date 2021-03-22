1f2e55dffa59faf9936b1d01d5972d5f81f86660
zqfang666 <zqfang321@163.com>
2021-03-22 22:49:48 +0800

    Update BankAMain.java
    修复demo 启动异常问题

--- dtm-springcloud-hw/bankA-service/src/main/java/com/huawei/banka/BankAMain.java
+++ dtm-springcloud-hw/bankA-service/src/main/java/com/huawei/banka/BankAMain.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableConfigurationProperties
 @SpringBootApplication(scanBasePackages = {"com.huawei"})
 public class BankAMain {
+    @Bean
+    public FailedEventListener failedEventListener() {
+        return new FailedEventListener();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BankAMain.class, args);
     }
